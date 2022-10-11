04099bb9b541e8a9d8e44e68c110ff787046256c
buptshin <65529669+buptshin@users.noreply.github.com>
2022-10-11 22:57:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -52,7 +52,7 @@ const BLOG = {
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || false, // 鼠标点击烟花特效
 
   // 悬浮挂件
-  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
+  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || false, // 是否显示宠物挂件
   WIDGET_PET_LINK:
     'https://github.com/xiazeyu/live2d-widget-models/blob/master/packages/live2d-widget-model-tororo/assets/tororo.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
